795170fd0445aa0f210138f3208bfe521bca8e0e
Alexander <mushtty@gmail.com>
2020-07-07 22:21:28 +0000

    I did a bunch of protocol_lab stuff

--- intro_c/read_example/read_example.c
+++ intro_c/read_example/read_example.c
@@ -29,10 +29,12 @@
 {
   // Variable setup
     char flag[50];
-    int i;    for (i = 0; i < 50; i++){
+    int i;
+    for (i = 0; i < 50; i++){
         flag[i] = 0;
     }  // Initialize serial
-    uart_init(UART2);  // Read characters from serial into a string until a newline is received
+    uart_init(UART2);
+    // Read characters from serial into a string until a newline is received
     i = 0;
     int ret;
     flag[i] = uart_read(UART2, BLOCKING, &ret);
@@ -40,5 +42,6 @@
         i++;
         flag[i] = uart_read(UART2, BLOCKING, &ret);
     }  // Send the full string back over serial
-    uart_write_str(UART2, flag);  return 0;
+    uart_write_str(UART2, flag);
+    return 0;
 }
