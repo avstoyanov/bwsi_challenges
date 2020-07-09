f2856c5247213def565d323da1cd571c1bdf86cc
Alexander <mushtty@gmail.com>
2020-07-09 19:43:26 +0000

    save

--- uart_c/uart_challenge/uart.c
+++ uart_c/uart_challenge/uart.c
@@ -19,6 +19,7 @@ void uart_init(uint8_t uart)
 {
   // Implement me!!
     UART2_CTL_R &= ~(UART_CTL_UARTEN); // 0x0000.0001
+    UART2_IBRD_R &= ~(UART_IBRD_DIVINT_M); UART2_FBRD_R &= ~(UART_FBRD_DIVFRAC_M);
     UART2_IBRD_R |= 0x0a; UART2_FBRD_R |= 0x36;
     UART2_LCRH_R |= UART_LCRH_WLEN_8; // 0x0000.0060
     UART2_LCRH_R &= ~(UART_LCRH_PEN); // 0x0000.0002
@@ -34,14 +35,13 @@ uint8_t uart_read(uint8_t uart, int blocking, int *read)
         while (UART2_FR_R & UART_FR_RXFE); // 0x00000010
         *read = 1;
         return UART2_DR_R & UART_DR_DATA_M; // 0x000000FF
+    }
+    if (UART2_FR_R & UART_FR_RXFF) { // 0x00000040
+        *read = 1;
+        return UART2_DR_R & UART_DR_DATA_M; // 0x000000FF
     } else {
-        if (UART2_FR_R & UART_FR_RXFF) { // 0x00000040
-            *read = 1;
-            return UART2_DR_R & UART_DR_DATA_M; // 0x000000FF
-        } else {
-            *read = 0;
-            return 0;
-        }
+        *read = 0;
+        return 0;
     }
 }
 
@@ -49,7 +49,7 @@ void uart_write(uint8_t uart, uint32_t data)
 {
   // Implement me!!
     while (UART2_FR_R & UART_FR_BUSY); // 0x00000008
-    UART2_DR_R = (data & UART_DR_DATA_M); // 0x000000FF
+    UART2_DR_R = (UART2_DR_R & ~(UART_DR_DATA_M)) | (data & UART_DR_DATA_M); // 0x000000FF
 }
 
 void uart_write_str(uint8_t uart, char *str) {
